33cabc330f68f461f74eb89722eccb5abf3c9545
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-14 15:24:57 -0800

    Add Home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-
 import React from 'react';
 import Header from './Header';
+import Home from './Home';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 };
