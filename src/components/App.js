f89836bcbb73054caa8134a9ea8693d087dfda63
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-14 15:27:54 -0800

    Add About component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import About from './About';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <About />
     </div>
   );
 };
