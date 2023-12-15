853c7d2708b8b73f3284f4b9da659c3c5af91b47
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-15 13:21:07 -0800

    Add TechParticles.jpg w/ animation & styles + modularize divs

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import techParticles from '../img/TechParticles.jpg';
 import './About.css';
 
 const About = () => {
   return (
     <div className="about-container" id="about">
-      <h2>Smoothed Particle Hydrodynamics</h2>
-      <p>
-        Smoothed Particle Hydrodynamics (SPH) is a computational method used for
-        simulating fluid flows. It represents fluid as a set of particles and
-        applies smoothed interpolation functions to calculate fluid properties at
-        each particle location.
-      </p>
-      <p>
-        SPH simulations are widely used in various fields, including fluid
-        dynamics, astrophysics, and computer graphics, providing a versatile
-        approach to model complex fluid behaviors.
-      </p>
+      <div className="about-hero-layout">
+        <div className="about-image-container">
+          <img src={techParticles} alt="Particles" />
+        </div>
+        <div className="about-text-container">
+          <h2 className='essence'>ESSENCE</h2>
+          <p>
+            Smoothed Particle Hydrodynamics (SPH) is a computational method used for
+            simulating fluid flows. It represents fluid as a set of particles and
+            applies smoothed interpolation functions to calculate fluid properties at
+            each particle location.
+          </p>
+          <p>
+            SPH simulations are widely used in various fields, including fluid
+            dynamics, astrophysics, and computer graphics, providing a versatile
+            approach to model complex fluid behaviors.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
