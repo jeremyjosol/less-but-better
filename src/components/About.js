24b15a19abe410b54c0317ab5bf2c4bea2cf4d58
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-15 13:28:11 -0800

    Update title + useEffect & useRef to render new animation feature

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import techParticles from '../img/TechParticles.jpg';
 import './About.css';
 
 const About = () => {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            titleRef.current.classList.add('active');
+          } else {
+            titleRef.current.classList.remove('active');
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    observer.observe(titleRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <div className="about-container" id="about">
       <div className="about-hero-layout">
@@ -10,7 +33,9 @@ const About = () => {
           <img src={techParticles} alt="Particles" />
         </div>
         <div className="about-text-container">
-          <h2 className='essence'>ESSENCE</h2>
+          <h2 ref={titleRef} className="essence fade-in">
+            ESSENCE
+          </h2>
           <p>
             Smoothed Particle Hydrodynamics (SPH) is a computational method used for
             simulating fluid flows. It represents fluid as a set of particles and
@@ -21,7 +46,7 @@ const About = () => {
             SPH simulations are widely used in various fields, including fluid
             dynamics, astrophysics, and computer graphics, providing a versatile
             approach to model complex fluid behaviors.
-          </p>
+          </p>        
         </div>
       </div>
     </div>
