abbefb673f989587672ef92dfed146f9e4978203
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-16 15:33:01 -0800

    Test video import w/ simulationOne SPH functionality

--- src/components/Sequence.js
+++ src/components/Sequence.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Sequence.css';
+import simulationOne from '../mp4/simulationOne.mp4';
 
 const Sequence = () => {
   return (
@@ -11,8 +12,14 @@ const Sequence = () => {
       <h1 className='sequence5'>Sequence</h1>
       <h1 className='sequence6'>Sequence</h1>
       <h1 className='sequence7'>Sequence</h1>
+
+      <video width="320" height="240" controls>
+        <source src={simulationOne} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
 
 export default Sequence;
+
