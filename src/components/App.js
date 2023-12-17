ae658b25d2c61a51b5216abece26791e077a0a86
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-17 00:35:16 -0800

    Add Simulation component + work on video w/ slider & styles

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import About from './About';
 import Sequence from './Sequence';
 import Void from './Void';
+import Simulation from './Simulation';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <div className='cuboid' id='void'>
         <Void />
       </div>
+      <Simulation />
     </div>
   );
 };
---
+++ src/components/Simulation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Slider from 'react-slick';
+import './Simulation.css';
+import simulationOne from '../mp4/simulationOne.mp4';
+import simulationTwo from '../mp4/simulationTwo.mp4';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const Simulation = () => {
+  const videos = [
+    { id: 1, src: simulationOne, title: 'Sequence 1' },
+    { id: 2, src: simulationTwo, title: 'Sequence 2'},
+  ];
+
+return (
+  <div className='simulation-container'>
+    <Slider {...settings}>
+        {videos.map((video) => (
+          <div key={video.id}>
+            <h1 className="sequence-heading">{video.title}</h1>
+            <div className="video-container">
+              <video width="500px" height="240px" controls>
+                <source src={video.src} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default Simulation;
