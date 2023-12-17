bc4f8da6f721dfbc3dcad6c36c0d2f3be33558c7
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-16 23:32:10 -0800

    Add new features & styles

--- src/components/Sequence.js
+++ src/components/Sequence.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import './Sequence.css';
+import { FaChevronDown } from "react-icons/fa";
 
 const Sequence = () => {
+  const voidRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (voidRef.current) {
+        const rect = voidRef.current.getBoundingClientRect();
+        const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight;
+        if (isInViewport) {
+          console.log('Section with ID "void" is in the viewport');
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToVoid = () => {
+    scroll.scrollTo(voidRef.current.offsetTop, {
+      duration: 500,
+      smooth: 'easeInOutQuart',
+      offset: -150,
+    });
+  };
+
   return (
-    <div className="sequence-container" id='sequence'>
-      <h1 className='sequence1'>Sequence</h1>
-      <h1 className='sequence2'>Sequence</h1>
-      <h1 className='sequence3'>Sequence</h1>
-      <h1 className='sequence4'>Sequence</h1>
-      <h1 className='sequence5'>Sequence</h1>
-      <h1 className='sequence6'>Sequence</h1>
-      <h1 className='sequence7'>Sequence</h1>
+    <div>
+      <div className="sequence-container" id='sequence'>
+        <h1 className='sequence1'>Sequence</h1>
+        <h1 className='sequence2'>Sequence</h1>
+        <h1 className='sequence3'>Sequence</h1>
+        <h1 className='sequence4'>Sequence</h1>
+        <h1 className='sequence5'>Sequence</h1>
+        <h1 className='sequence6'>Sequence</h1>
+        <h1 className='sequence7'>Sequence</h1>
+        <div ref={voidRef} id="void" />
+      </div>
+      <div className="sequence-down-icon" onClick={scrollToVoid}>
+        <FaChevronDown />
+      </div>
     </div>
   );
 }
 
 export default Sequence;
+
 
