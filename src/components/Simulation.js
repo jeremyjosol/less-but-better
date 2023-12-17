c9e75f6fb9b669e8f9126d6d9ddd80fb249b37b7
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-17 12:05:58 -0800

    Update component + add icon & new video

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -3,8 +3,10 @@ import Slider from 'react-slick';
 import './Simulation.css';
 import simulationOne from '../mp4/simulationOne.mp4';
 import simulationTwo from '../mp4/simulationTwo.mp4';
+import simulationThree from '../mp4/simulationThree.mp4';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { MdHub } from "react-icons/md";
 
 const settings = {
   dots: true,
@@ -18,11 +20,12 @@ const Simulation = () => {
   const videos = [
     { id: 1, src: simulationOne, title: '' },
     { id: 2, src: simulationTwo, title: ''},
+    { id: 3, src: simulationThree, title: ''},
   ];
 
 return (
   <div className='simulation-container' id='simulation'>
-    <h1 className='prototype'>Prototype</h1>
+    <h1 className='prototype'><MdHub className='prototype-icon' /> Prototype</h1>
     <Slider {...settings}>
         {videos.map((video) => (
           <div key={video.id}>
