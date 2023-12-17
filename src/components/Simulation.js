6e1319ce89ecf1f92f5264a5cfab497fe3f36d1d
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-17 01:24:28 -0800

    Add video replay handling per slide clicked - only works for first video

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Slider from 'react-slick';
 import './Simulation.css';
 import simulationOne from '../mp4/simulationOne.mp4';
@@ -12,22 +12,50 @@ const settings = {
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: false, 
 };
 
 const Simulation = () => {
+  const sliderRef = useRef(null);
   const videos = [
-    { id: 1, src: simulationOne, title: 'Sequence 1' },
-    { id: 2, src: simulationTwo, title: 'Sequence 2'},
+    { id: 1, src: simulationOne, title: 'PHASE ONE' },
+    { id: 2, src: simulationTwo, title: '' },
   ];
 
-return (
-  <div className='simulation-container' id='simulation'>
-    <Slider {...settings}>
+  const handleBeforeChange = (oldIndex, newIndex) => {
+    const video = document.getElementById(`video-${oldIndex + 1}`);
+    if (video) {
+      video.pause();
+      video.currentTime = 0; 
+    }
+  };
+
+  const handleAfterChange = (index) => {
+    const video = document.getElementById(`video-${index + 1}`);
+    if (video) {
+      video.play();
+    }
+  };
+
+  return (
+    <div className='simulation-container' id='simulation'>
+      <Slider
+        {...settings}
+        ref={sliderRef}
+        beforeChange={handleBeforeChange}
+        afterChange={handleAfterChange}
+      >
         {videos.map((video) => (
           <div key={video.id}>
             <h1 className="sequence-heading">{video.title}</h1>
             <div className="video-container">
-              <video width="500px" height="240px" controls>
+              <video
+                id={`video-${video.id}`}
+                width="500px"
+                height="240px"
+                controls
+                preload="auto"
+              >
                 <source src={video.src} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
@@ -37,6 +65,6 @@ return (
       </Slider>
     </div>
   );
-}
+};
 
 export default Simulation;
