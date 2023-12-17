cc5c164cb806735b70608f51dd507d3f1ccc29aa
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-17 03:01:00 -0800

    Implement Sequence effects per element

--- src/components/Sequence.js
+++ src/components/Sequence.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-scroll';
 import './Sequence.css';
-import { FaChevronDown } from "react-icons/fa";
+import { FaChevronDown } from 'react-icons/fa';
 
 const Sequence = () => {
   const voidRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (voidRef.current) {
         const rect = voidRef.current.getBoundingClientRect();
         const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+        if (isInViewport && !isVisible) {
+          setIsVisible(true);
+        } else if (!isInViewport && isVisible) {
+          setIsVisible(false);
+        }
       }
     };
 
@@ -19,18 +26,18 @@ const Sequence = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [isVisible]);
 
   return (
     <div>
-      <div className="sequence-container" id='sequence'>
-        <h1 className='sequence1'>Sequence</h1>
-        <h1 className='sequence2'>Sequence</h1>
-        <h1 className='sequence3'>Sequence</h1>
-        <h1 className='sequence4'>Sequence</h1>
-        <h1 className='sequence5'>Sequence</h1>
-        <h1 className='sequence6'>Sequence</h1>
-        <h1 className='sequence7'>Sequence</h1>
+      <div className={`sequence-container ${isVisible ? 'sequence-visible' : ''}`} id="sequence">
+        <h1 className="sequence1">Sequence</h1>
+        <h1 className="sequence2">Sequence</h1>
+        <h1 className="sequence3">Sequence</h1>
+        <h1 className="sequence4">Sequence</h1>
+        <h1 className="sequence5">Sequence</h1>
+        <h1 className="sequence6">Sequence</h1>
+        <h1 className="sequence7">Sequence</h1>
         <div ref={voidRef} id="void" />
       </div>
       <Link
@@ -45,7 +52,7 @@ const Sequence = () => {
       </Link>
     </div>
   );
-}
+};
 
 export default Sequence;
 
