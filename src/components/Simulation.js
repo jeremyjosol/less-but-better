f19610ea36d0430cf14dfcd0febdc2a88ac40d6d
Jeremy Josol <jeremyjosol@gmail.com>
2023-12-17 13:31:25 -0800

    Build out component + add new videos

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -6,10 +6,13 @@ import simulationTwo from '../mp4/simulationTwo.mp4';
 import simulationThree from '../mp4/simulationThree.mp4';
 import simulationFour from '../mp4/simulationFour.mp4';
 import simulationFive from '../mp4/simulationSmoothedOne.mp4';
-import simulationSix from '../mp4/simulationSmoothedTwo.mp4';
+import simulationSix from '../mp4/simulationSmoothedTwo.mov';
+import simulationSeven from '../mp4/simulationInteractOne.mov';
+import simulationEight from '../mp4/simulationInteractTwo.mov';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { MdHub } from 'react-icons/md';
+import { MdOutlineLensBlur } from "react-icons/md";
 
 const prototypeSettings = {
   dots: true,
@@ -37,6 +40,8 @@ const prototypeVideos = [
 const kaizenVideos = [
   { id: 1, src: simulationFive, title: '' },
   { id: 2, src: simulationSix, title: '' },
+  { id: 3, src: simulationSeven, title: ''},
+  { id: 4, src: simulationEight, title: ''},
 ];
 
 const Simulation = () => {
@@ -49,7 +54,6 @@ const Simulation = () => {
         <Slider {...prototypeSettings}>
           {prototypeVideos.map((video) => (
             <div key={video.id}>
-              <h1 className="sequence-heading">{video.title}</h1>
               <div className="video-container">
                 <video width="500px" height="240px" controls>
                   <source src={video.src} type="video/mp4" />
@@ -63,12 +67,11 @@ const Simulation = () => {
 
       <div className="slider-container">
         <h1 className="prototype">
-          <MdHub className="prototype-icon" /> Kaizen
+          <MdOutlineLensBlur className="prototype-icon" /> Kaizen
         </h1>
         <Slider {...kaizenSettings}>
           {kaizenVideos.map((video) => (
             <div key={video.id}>
-              <h1 className="sequence-heading">{video.title}</h1>
               <div className="video-container">
                 <video width="500px" height="240px" controls>
                   <source src={video.src} type="video/mp4" />
